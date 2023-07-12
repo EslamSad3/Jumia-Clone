1742fccf4142bb35127eefbff5238fa6593c0361
Eslam Saad <106920768+EslamSad3@users.noreply.github.com>
2023-07-12 16:39:22 +0300

    Search

--- src/components/SearchForProducts/SearchProducts.jsx
+++ src/components/SearchForProducts/SearchProducts.jsx
@@ -8,7 +8,21 @@ function SearchProducts() {
             <SearchSIdeBar/>
         </div>
         <div className="col-8 my-4 p-3 bg-white">
-            products
+            <div className='d-flex justify-content-between'>
+                <br />
+                <div className="dropdown cursor-pointer">
+                    <span className=" dropdown-toggle" id="triggerId" data-bs-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false">
+                                <h6 className='d-inline'>Sort By </h6> <span>: Popularity</span>
+                            </span>
+                    <div className="dropdown-menu sorted" aria-labelledby="triggerId">
+                        <a className="dropdown-item" href="#">Popularity</a>
+                        <a className="dropdown-item" href="#">Newest Arrivals</a>
+                        <a className="dropdown-item" href="#">Price : Low To High</a>
+                        <a className="dropdown-item" href="#">Product Rating</a>
+                    </div>
+                </div>
+            </div>
         </div>
 
     </div>
