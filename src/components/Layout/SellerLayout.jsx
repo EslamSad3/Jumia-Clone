55c492da429830a23b739dd30d04b420cbad8a1c
aliragab1 <aliragab121@gmail.com>
2023-07-23 18:16:18 +0300

    gghhhh

--- src/components/Layout/SellerLayout.jsx
+++ src/components/Layout/SellerLayout.jsx
@@ -3,17 +3,17 @@ import { Outlet } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
 import SideBar from '../sidebar/SideBar';
 
-import React from 'react'
 
-function SellerLayout({userData}) {
+
+function SellerLayout({ userData }) {
     return <>
 
-    <SideBar userData={userData} />
-    <ToastContainer />
-    <Outlet >
-   </Outlet >
-   
-</>
+        <SideBar userData={userData} />
+        <ToastContainer />
+        <Outlet >
+        </Outlet >
+
+    </>
 }
 
 export default SellerLayout
