3988fb0c852d8f51c46cf3b956064f278ea1b11b
Eslam Saad <106920768+EslamSad3@users.noreply.github.com>
2023-07-23 14:56:47 +0300

    Adding products

--- src/components/protectedRoute/ProtectedRoute.jsx
+++ src/components/protectedRoute/ProtectedRoute.jsx
@@ -1,10 +1,12 @@
+import jwtDecode from 'jwt-decode'
 import React from 'react'
 import { Navigate } from 'react-router'
 
 export default function ProtectedRoute(prpos) {
-    // const navigate = useNavigate()
-    if (localStorage.getItem('UserToken') == null) {
-        return <Navigate to={'/login'} />
+    const tokenData = localStorage.getItem('UserToken')
+    const auth = jwtDecode(tokenData)
+    if (localStorage.getItem('UserToken') == null || auth.role !== "seller") {
+        return <Navigate to={'/'} />
     }
     else {
 
